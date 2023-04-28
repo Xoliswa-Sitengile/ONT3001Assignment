e0d273b9009b750ade7c31462efea7dc2fe26037
s219215677 <s219215677@M104-41>
2023-04-28 10:57:31 +0200

    Add project files.

---
+++ ONT3001Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ONT3001Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ONT3001Assignment/Models/Fighters.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System;
+namespace ONT3001Assignment.Models
+{
+    public class Fighters
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string BenderName { get; set; }
+        [Required]
+        public string Origins { get; set;}
+        [Required] 
+        public string Age { get;set; }
+        [Required]
+        public string Ability { get; set; }
+    }
+}
